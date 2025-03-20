88fb77595b03059e17be9fdffa28c353d3b32570
MaxWolmer <mwtj1000@gmail.com>
2025-03-20 23:22:49 +0100

    farveændringer på Inceptiondeck og Index siderne

---
+++ Timeline test/Pages/Inceptiondeck.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Timeline_test.Pages
+{
+    public class InceptiondeckModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
